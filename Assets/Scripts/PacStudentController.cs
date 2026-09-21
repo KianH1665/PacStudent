c4f449ca36c580075c19d245fdfce6fadebf48f5
KianH1665 <kian.hirani@gmail.com>
2026-09-21 14:04:57 +1000

    Finished movement band and added preview section to scene so all sprits and animations are displayed properly

--- Assets/Scripts/PacStudentController.cs
+++ Assets/Scripts/PacStudentController.cs
@@ -31,10 +31,6 @@ public class PacStudentController : MonoBehaviour
             targetIndex = (targetIndex + 1) % corners.Length;
             BeginMove();
         }
-
-        if (Time.deltaTime > 0.1) {
-            Debug.Log(Time.deltaTime);
-        }
     }
 
     void BeginMove() {
